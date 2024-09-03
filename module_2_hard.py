352247e97d8196f114dbda8fb7607cf4c3c17b1e
BKazaryan <borislav.kazaryan1999@gmail.com>
2024-09-04 02:30:39 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,9 +1,9 @@
-def code(numbers):
+def code(n):
     result = []
-    for i in range(1, numbers + 1):
-        for j in range(i + 1, numbers + 1):
+    for i in range(1, n + 1):
+        for j in range(i + 1, n + 1):
             sum_ = i + j
-            if numbers % sum_ == 0:
+            if n % sum_ == 0:
                 result.append(i)
                 result.append(j)
     return result
